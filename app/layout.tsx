e4de971068061f6045d817b7fa9b10548a23628c
Mekd422 <mekdes.asefa-ug@aau.edu.et>
2025-08-13 19:13:06 +0300

    minor fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Mona_Sans, Mona_Sans } from "next/font/google";
+import { Mona_Sans } from "next/font/google";
 import "./globals.css";
 
-const Mona_Sans = Mona_Sans({
+const monaSans = Mona_Sans({
   variable: "--font-mona-sans",
   subsets: ["latin"],
 });
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body
-        className={`${Mona_Sans.className} antialiased`}
+        className={`${monaSans.className} antialiased`}
       >
         {children}
       </body>
