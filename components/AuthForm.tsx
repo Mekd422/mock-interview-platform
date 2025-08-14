9bfcd32f4610e9eb61a9f3cf036ba4e34d0ba36d
Mekd422 <mekdes.asefa-ug@aau.edu.et>
2025-08-14 16:03:27 +0300

    |

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -21,7 +21,7 @@ const formSchema = z.object({
   username: z.string().min(2).max(50),
 })
 
-const AuthForm = () => {
+const AuthForm = ({type} : {type: FormType}) => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -35,6 +35,8 @@ const AuthForm = () => {
     // ✅ This will be type-safe and validated.
     console.log(values)
   }
+
+  const isSignIn = type === "sign-in"
   return (
     <div className="card-border lg:min-w-[566px]">
       <div className="flex flex-col gap-6 card py-14 px-10">
@@ -51,10 +53,21 @@ const AuthForm = () => {
       
       <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6 mt-4 form">
+
+        {!isSignIn && <p>Name</p>}
+        <p>Email</p>
+        <p>Password</p>
         
-        <Button type="submit">Submit</Button>
+        <Button className="btn" type="submit">{isSignIn ? 'Sign in' : 'create an Account'}</Button>
       </form>
     </Form>
+
+    <p className="text-center">
+      {isSignIn ? 'No account Yet?' : 'Have an account already?'}
+      <Link href={!isSignIn ? '/sign-in' : '/sign-up'} className="font-bold text-user-primary ml-1">
+      {!isSignIn ? 'Sign in' : 'Sign up'}
+      </Link>
+    </p>
     </div>
     </div>
   )
