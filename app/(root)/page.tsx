57de87520bf9b7ab694173d02282bb21c443a643
Mekd422 <mekdes.asefa-ug@aau.edu.et>
2025-08-16 00:47:56 +0300

    nav and header

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,30 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
 
 
-function Home() {
+
+const Page = () => {
   return (
-    <div>Home page</div>
+    <>
+      <section className="card-cta" >
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
+          <p className="text-lg">
+            Practice on real interview questions, get instant feedback
+          </p>
+
+          <Button asChild className="btn-primary max-sm:w-full">
+          <Link href= "/interview" >Start an Interview</Link>
+        </Button>
+        </div>
+
+        <Image src="/robot.png" alt="robo-dude" width={400} height={400} className="max-sm:hidden"/>
+
+        
+        
+      </section>
+    </>
   );
 }
-export default Home;
+export default Page;
