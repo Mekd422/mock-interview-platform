db5ac90c2daf50d5c76772af780532c11d269b09
Mekd422 <mekdes.asefa-ug@aau.edu.et>
2025-08-18 18:41:41 +0300

    updated authform

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,4 +1,8 @@
 "use client"
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { signUp } from "@/lib/actions/auth.action";
+import { auth } from "@/firebase/client";
+
 
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
@@ -34,11 +38,31 @@ const AuthForm = ({type} : {type: FormType}) => {
   })
  
   
-function onSubmit(values: z.infer<typeof formSchema>) {
+const onSubmit = async  (values: z.infer<typeof formSchema>) => {
     try {
       if(type === "sign-up"){
-        toast.success("Account created successfully")
-        router.push('/sign-in')
+        const { name, email, password } = values;
+
+        const userCredential = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+
+        const result = await signUp({
+          uid: userCredential.user.uid,
+          name: name!,
+          email,
+          password,
+        });
+
+        if (!result.success) {
+          toast.error(result.message);
+          return;
+        }
+
+        toast.success("Account created successfully. Please sign in.");
+        router.push("/sign-in");
       } else {
         toast.success("Signed in successfully")
         router.push('/')
--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -26,7 +26,7 @@ export async function signUp(params: SignUpParams) {
       success: true,
       message: "Account created successfully. Please sign in.",
     };
-  } catch (error: any) {
+  } catch (error: any ) {
     console.error("Error creating user:", error);
 
     // Handle Firebase specific errors
